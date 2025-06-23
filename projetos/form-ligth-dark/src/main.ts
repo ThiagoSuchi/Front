d96f33e4d65c3ff851ce9b3c6a6d1638859f1f58
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-23 17:15:16 -0400

    Função sidebar criada para deixar o código flexívo...

--- projetos/form-ligth-dark/src/main.ts
+++ projetos/form-ligth-dark/src/main.ts
@@ -3,12 +3,11 @@ import './style.css'
 import { camposValidacao } from './service/camposValidadcao';
 import { darkMode } from './service/darkMode';
 import { limparForm } from './service/limparForm';
-import { responseGet, responsePost } from './routes/usuarios.routes';
+import { responsePost } from './routes/usuarios.routes';
+import { sidebarButtons } from './service/sidebar';
 
 const btnDark = document.querySelector(".btn-dark") as HTMLButtonElement;
 const form = document.querySelector("#form") as HTMLFormElement;
-const btnForm = document.querySelector('.btn-form') as HTMLButtonElement;
-const btnList = document.querySelector('.btn-list') as HTMLButtonElement;
 
 // Capiturando os dados do formulário, e criando um usuário na api
 form.addEventListener('submit', async (e) => {
@@ -43,33 +42,4 @@ btnDark.addEventListener('click', () => {
 });
 
 // Menu lateral esquerdo
-async function sidebarButtons(): Promise<void> {
-    const form = document.querySelector('.formulario') as HTMLSelectElement;
-    const lista = document.querySelector('.lista') as HTMLSelectElement;
-
-    btnForm.addEventListener('click', () => {
-        form.style.display = 'block';
-        lista.style.display = 'none'    
-    })
-
-    btnList.addEventListener('click', async () => {
-        form.style.display = 'none';
-        lista.style.display = 'block'
-
-        const usuarios = await responseGet()
-
-        lista.innerHTML = `
-            <div>
-                ${JSON.stringify(usuarios, null, 2)}
-            </div>
-        `
-    })
-}
-
-// Lógica simples para manter por padrão o estilo aplicado ao sidebar:focus
-window.addEventListener('DOMContentLoaded', () => {
-    const btnForm = document.querySelector('.btn-form') as HTMLButtonElement;
-    btnForm.focus();
-});
-
-sidebarButtons()
+sidebarButtons();
